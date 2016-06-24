6f47ec93fd0f22c7e09d938e4854af3fdd777328
Chris Jones <chris@axismedtech.com>
2016-06-24 12:00:03 -0700

    work

---
+++ iOSDemo/Action/ActionBase.swift
@@ -0,0 +1,72 @@
+//
+// Created by Chris Jones on 6/24/16.
+// Copyright (c) 2016 ClickHandlerIO. All rights reserved.
+//
+
+import Foundation
+
+class ActionBase<REQ, RESP> {
+    var request: REQ?
+
+    init() {
+    }
+
+    func actionType() -> ActionType {
+        preconditionFailure("This method must be overridden")
+    }
+
+    func run(request: REQ) -> RESP {
+        preconditionFailure("This method must be overridden")
+    }
+}
+
+class BaseDbAction<REQ, RESP>: ActionBase<REQ, RESP> {
+
+    func getDbConn() -> AnyObject {
+        preconditionFailure("This method must be overridden")
+    }
+
+    func run(request: REQ) {
+        run(request, conn: "")
+    }
+
+    func run(request: REQ, conn: AnyObject) {
+        preconditionFailure("This method must be overridden")
+    }
+}
+
+class WriteDbAction<REQ, RESP>: BaseDbAction<REQ, RESP> {
+    override func getDbConn() -> AnyObject {
+        return ""// TODO
+    }
+}
+
+class ReadUncommittedDbAction<REQ, RESP>: BaseDbAction<REQ, RESP> {
+    override func getDbConn() -> AnyObject {
+        return "" // TODO
+    }
+}
+
+class LoginAction: ActionBase<Request, Response> {
+
+    override init() {
+    }
+
+    override func actionType() -> ActionType {
+        return .General
+    }
+
+    override func run(request: Request) -> Response {
+        // do work here
+        return Response()
+    }
+}
+
+public struct Request {
+    var username: String?
+    var password: String?
+}
+
+public struct Response {
+    var code: String?
+}
---
+++ iOSDemo/Action/ActionType.swift
@@ -0,0 +1,12 @@
+//
+// Created by Chris Jones on 6/24/16.
+// Copyright (c) 2016 ClickHandlerIO. All rights reserved.
+//
+
+import Foundation
+
+enum ActionType {
+    case General
+    case DatabaseWrite
+    case DatabaseReadUncommitted
+}
