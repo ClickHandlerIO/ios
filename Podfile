c50d6d98c7e3964108db3b1b2c25e38eafe0a1ef
Chris Jones <chris@axismedtech.com>
2016-06-24 11:33:23 -0700

    initial

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'iOSDemo' do
+  pod 'SwiftyJSON', '~> 2.3'
+  pod 'SQLite.swift', '~> 0.10.1'
+end
---
+++ iOSDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iOSDemo
+//
+//  Created by Chris Jones on 6/24/16.
+//  Copyright © 2016 ClickHandlerIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+    }
+}
+
