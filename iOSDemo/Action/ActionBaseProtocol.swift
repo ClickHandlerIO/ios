4821edb9565d60f9c4ea6a67a366a7726212fd7a
Chris Jones <chris@axismedtech.com>
2016-06-27 21:53:54 -0600

    working

--- iOSDemo/Action/ActionBaseProtocol.swift
+++ iOSDemo/Action/ActionBaseProtocol.swift
@@ -14,27 +14,6 @@ protocol ActionProtocol {
     static func run(request: Request) -> Response
 }
 
-//struct AnyAction<REQ, RESP>: ActionProtocol {
-//    typealias Request = REQ
-//    typealias Response = RESP
-//
-//    let _actionType: () -> ActionType
-//    let _run: (REQ) -> RESP
-//
-//    init<Base:ActionProtocol where Base.Request == REQ, Base.Response == RESP>(base: Base) {
-//        _actionType = base.actionType
-//        _run = base.run
-//    }
-//
-//    func actionType() -> ActionType {
-//        return _actionType()
-//    }
-//
-//    func run(request: Request) -> Response {
-//        return _run(request)
-//    }
-//}
-
 struct LoginAction: ActionProtocol {
     typealias Request = LoginAction.Req
     typealias Response = LoginAction.Resp
@@ -55,7 +34,7 @@ struct LoginAction: ActionProtocol {
             }
         }
 
-        return Response(.FAILURE)
+        return Response(.FAILED)
     }
 
     struct Req {
@@ -71,54 +50,10 @@ struct LoginAction: ActionProtocol {
         }
 
         enum Code {
+            case CANCELLED
+            case FAILED
             case SUCCESS
-            case FAILURE
         }
-
     }
 
 }
-
-
-/*
-struct LoginAction: Action {
-    typealias Request = LoginAction.Req
-    typealias Response = LoginAction.Resp
-
-    static func actionType() -> ActionType {
-        return .General
-    }
-
-    static func run(request: Request) -> Response {
-        // todo guard against nil username and pw
-        // todo mulitple unwrap and guard?
-
-        if let username = request.username {
-            print("hello")
-            if let password = request.password {
-                print("there")
-                return Response(.SUCCESS)
-            }
-        }
-
-        return Response(.FAILURE)
-    }
-
-    struct Req {
-        var username: String?
-        var password: String?
-    }
-
-    struct Resp {
-        var code: Code
-
-        init(_ code: Code) {
-            self.code = code
-        }
-
-        enum Code {
-            case SUCCESS
-            case FAILURE
-        }
-    }
-}*/
